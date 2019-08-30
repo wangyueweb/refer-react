a6a4d04e573e68631695e5dc0416a46c4069e7f1
ClownLee <lwentao@hotmail.com>
2019-08-30 13:12:03 +0800

    测试 github pull

--- mock/user.js
+++ mock/user.js
@@ -7,7 +7,7 @@ export default {
   'GET /api/currentUser': {
     name: 'Serati Ma',
     avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
-    userid: '00000001',
+    userid: '1',
     email: 'antdesign@alipay.com',
     signature: '海纳百川，有容乃大',
     title: '交互专家',
